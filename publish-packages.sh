d4a24a4db06e7cfa224f43581637f106c473f89c
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-04-08 16:13:19 -0400

    Incrementing alpha version.

--- publish-packages.sh
+++ publish-packages.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 NPM_TAG=${1};
+NOT_DRY_RUN=
 
 if [ -z "${NPM_TAG}" ]; then {
 	echo "A tag is required.";
@@ -58,8 +59,14 @@ ls packages | while read PACKAGE; do {
 	cd "packages/${PACKAGE}"
 	echo -e "\033[33mChanged files in \033[1m${PACKAGE}:\033[0m";
 	npm diff --tag ${NPM_TAG} --diff-name-only || ( cd ../.. && continue )
-	set -x
-	npm publish --tag ${NPM_TAG}
-	set +x
+	if [[ "${NOT_DRY_RUN:-}" == "real" ]]; then
+		set -x
+		xyz publish --tag ${NPM_TAG}
+		set +x
+	else
+		set -x
+		npm publish --tag ${NPM_TAG} --dry-run
+		set +x
+	fi
 	cd "../.."
 }; done;
