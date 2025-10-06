47b3cff11eee63b955da0827e44f98b078984bcb
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-10-06 03:24:29 -0400

    Fixing intl

--- packages/intl/index.mjs
+++ packages/intl/index.mjs
@@ -1,17 +1,24 @@
 import intl8_4 from './8.4.mjs';
 import intl8_3 from './8.3.mjs'; 
-// import intl8_2 from './8.2.mjs'; 
-// import intl8_1 from './8.1.mjs'; 
-// import intl8_0 from './8.0.mjs'; 
+import intl8_2 from './8.2.mjs'; 
+import intl8_1 from './8.1.mjs'; 
+import intl8_0 from './8.0.mjs'; 
 
 const versionTable = {
 	'8.4': intl8_4,
 	'8.3': intl8_3,
-	// '8.2': intl8_2,
-	// '8.1': intl8_1,
-	// '8.0': intl8_0,
+	'8.2': intl8_2,
+	'8.1': intl8_1,
+	'8.0': intl8_0,
 };
 
 export default {
-	getLibs: php => versionTable[php.phpVersion]
+	getLibs: php => versionTable[php.phpVersion],
+	getFiles: () => [
+		{
+			name: 'icudt72l.dat',
+			path: '/preload/icudt72l.dat',
+			url: new URL('./icudt72l.dat', import.meta.url)
+		},
+	]
 };
