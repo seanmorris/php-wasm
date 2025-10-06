42fa5c0ea4769c2e07cd6e691e49eac8ea8aa492
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-10-06 10:53:26 -0400

    Typo in phar/8.0.mjs

--- packages/phar/8.0.mjs
+++ packages/phar/8.0.mjs
@@ -3,7 +3,7 @@ const url = new URL(importMeta.url ?? 'http://example.com#this-is-an-error-supre
 const ini = !!(Number( url.searchParams.get('ini') ?? true ));
 
 export const getLibs = () => [
-    {url: new URL(`./php8.0phar.so`, import.meta.url), ini},
+    {url: new URL(`./php8.0-phar.so`, import.meta.url), ini},
 ];
 
 export default {getLibs};
