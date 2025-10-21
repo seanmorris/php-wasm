3ad732871916792a5fa8cf2a7f4a48d98f253d73
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-10-21 01:20:24 -0400

    stdlib & numeric prop access from JS

---
+++ test/stdlib.mjs
@@ -0,0 +1,31 @@
+import { test } from 'node:test';
+import { strict as assert } from 'node:assert';
+import { microtime, date, sprintf, json_decode } from '../packages/php-wasm/stdlib/8.2-node.mjs';
+
+test('Can use PHP functions that return NUMBERS in JS', () => {
+	assert.equal( microtime(true), Date.now() / 1000 );
+});
+
+test('Can use PHP functions that return STRINGS in JS', () => {
+	assert.equal( date('Y-m-d h:i:s', 0), '1970-01-01 12:00:00' );
+	assert.equal( sprintf('A %s is %d %s.', 'dozen', 12, 'eggs'), 'A dozen is 12 eggs.');
+});
+
+test('Can use PHP functions that return OBJECTS in JS', () => {
+	const x = json_decode(JSON.stringify({a: 'test'}));
+	assert.equal( x.a, 'test' );
+});
+
+test('Can use PHP functions that return ARRAYS in JS', () => {
+	const x = json_decode(JSON.stringify([0,1,2]));
+
+	assert.equal( x[0], 0 );
+	assert.equal( x[1], 1 );
+	assert.equal( x[2], 2 );
+});
+
+test('Can use PHP functions that return ASSOC ARRAYS in JS', () => {
+	const x = json_decode(JSON.stringify({"a": 1}), 1);
+
+	assert.equal( x["a"], 1 );
+});
