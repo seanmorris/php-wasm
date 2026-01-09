b00348ea4c136eb5b3a3aca973c1e984dcd9b404
Sean Morris <640101+seanmorris@users.noreply.github.com>
2026-01-08 19:32:52 -0500

    node-cgi-test bugfix for static & shared builds

--- test/node-cgi-test.sh
+++ test/node-cgi-test.sh
@@ -11,7 +11,7 @@ set -e;
 HOST_DIR="${PWD}"
 MOUNTED_DIR="/app"
 
-docker run --rm --name php-cgi-wasm-test-node -e PHP_VERSION=${PHP_VERSION} -p ${PORT}:3003 -v ${HOST_DIR}:${MOUNTED_DIR} -w /app node:24 npm start --prefix demo-node/ &
+docker run --rm --name php-cgi-wasm-test-node -e PHP_VERSION=${PHP_VERSION} -e BUILD_TYPE=${BUILD_TYPE} -p ${PORT}:3003 -v ${HOST_DIR}:${MOUNTED_DIR} -w /app node:24 npm start --prefix demo-node/ &
 trap "docker kill php-cgi-wasm-test-node" 0;
 
 set +x;
